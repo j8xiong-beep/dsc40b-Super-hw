f5a31148d674dab38b275960e95ae9349bb11621
Jingyun Xiong <jing@JingyundeMacBook-Pro.local>
2026-06-09 16:04:39 -0700

    super hw

---
+++ min_ell_theta.py
@@ -0,0 +1,30 @@
+def learn_theta(data, colors):
+    '''
+    Finds theta that is larger than all blue and less than all red.
+    '''
+    # TODO: Implement the learn_theta function
+    return None
+
+
+def compute_ell(data, colors, theta):
+    '''
+    Computes the loss function L(theta) for a given theta.
+    '''
+    # TODO: Implement the compute_ell function
+    return None
+
+
+def minimize_ell(data, colors):
+    '''
+    Finds theta that minimizes the loss function L(theta) using quadratic time complexity.
+    '''
+    # TODO: Implement the minimize_ell function
+    return None
+
+
+def minimize_ell_sorted(data, colors):
+    '''
+    Finds theta that minimizes the loss function L(theta) in linear time.
+    '''
+    # TODO: Implement the minimize_ell_sorted function
+    return None
---
+++ slc.py
@@ -0,0 +1,9 @@
+import dsc40graph
+
+def slc(graph, d, k):
+    '''
+    Performs single linkage clustering using Kruskal's algorithm.
+    '''
+    # TODO: Implement the slc function
+
+    return frozenset()
