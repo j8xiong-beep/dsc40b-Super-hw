b5211d3c5094259794eb3f42bdd62ea7e368fc9d
j8xiong-beep <j8xiong@ucsd.edu>
2026-06-09 16:39:46 -0700

    Update slc.py

--- slc.py
+++ slc.py
@@ -4,6 +4,31 @@ def slc(graph, d, k):
     '''
     Performs single linkage clustering using Kruskal's algorithm.
     '''
-    # TODO: Implement the slc function
+    clusters = [{node} for node in graph.nodes]
 
-    return frozenset()
+    edges = list(graph.edges)
+    edges.sort(key=d)
+
+    for edge in edges:
+        if len(clusters) == k:
+            break
+
+        u, v = edge
+
+        u_cluster = None
+        v_cluster = None
+
+        for cluster in clusters:
+            if u in cluster:
+                u_cluster = cluster
+            if v in cluster:
+                v_cluster = cluster
+
+        if u_cluster != v_cluster:
+            new_cluster = u_cluster | v_cluster
+
+            clusters.remove(u_cluster)
+            clusters.remove(v_cluster)
+            clusters.append(new_cluster)
+
+    return frozenset(frozenset(cluster) for cluster in clusters)
